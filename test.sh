5cd9362050c8940cac4b0d9ffaf9dffcdd125edf
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-24 19:57:57 +0100

    Cleanup

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 
 echo 'BBD0671EB6682D5050508AF7E3D9A464BEFF1A6D' >  pwned-passwords-ordered-2.0.txt
 echo 'F0D4031B7FBA0204D7DFF0D63198FA9369AE7073' >> pwned-passwords-ordered-2.0.txt
-touch pwned-passwords-update-1.txt
-echo "pwned-passwords-update-2.txt will be downloaded by container"
 
 docker run -v $(pwd):/data pwned-passwords CirclePassword | grep -c "Oh no"
 docker run -v $(pwd):/data pwned-passwords NotFound
