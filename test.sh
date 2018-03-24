86bcc24d90ad2b5c9c476be7ff8abfaeb3463efe
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-24 19:56:11 +0100

    Update pwned-passwords-ordered-2.0.txt

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-echo 'BBD0671EB6682D5050508AF7E3D9A464BEFF1A6D' >  pwned-passwords-1.0.txt
-echo 'F0D4031B7FBA0204D7DFF0D63198FA9369AE7073' >> pwned-passwords-1.0.txt
+echo 'BBD0671EB6682D5050508AF7E3D9A464BEFF1A6D' >  pwned-passwords-ordered-2.0.txt
+echo 'F0D4031B7FBA0204D7DFF0D63198FA9369AE7073' >> pwned-passwords-ordered-2.0.txt
 touch pwned-passwords-update-1.txt
 echo "pwned-passwords-update-2.txt will be downloaded by container"
 
